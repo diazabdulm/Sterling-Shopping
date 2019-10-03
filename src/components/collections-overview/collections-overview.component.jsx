628ff9c88002caa19174a1792cc5dd5341efedcf
Abdul <diazabdulm@gmail.com>
2019-10-03 04:19:01 -0400

    moved directory and shop data to redux

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
+
+import CollectionPreview from "../collection-preview/collection-preview.component";
+
+import "./collections-overview.styles.scss";
+
+const CollectionsOverview = ({ collections }) => (
+  <div className="collections-overview">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollectionsForPreview
+});
+
+export default connect(mapStateToProps)(CollectionsOverview);
